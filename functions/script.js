a6b09be761bedeaa004b4f9e9dd05d0da750b3a3
Tadashi Okuno <tadokuno@gmail.com>
2024-08-22 23:10:56 +0900

    v1.1.1

--- functions/script.js
+++ functions/script.js
@@ -41,23 +41,23 @@ export async function omuIndexMain(stationName) {
 
 // コマンドライン引数から駅名を取得
 
-console.log("omuIndexMain Start")
-const stationName = process.argv[2];
+//console.log("omuIndexMain Start")
+//const stationName = process.argv[2];
 
-if (!stationName) {
-    console.error('駅名を指定してください。');
-    process.exit(1);
-}
+//if (!stationName) {
+//    console.error('駅名を指定してください。');
+//    process.exit(1);
+//}
 
 // メイン処理
-(async () => {
-  try {
-    const result = await omuIndexMain(stationName); // 戻り値は表示する文字列
-    console.log(result);
-  } catch (error) {
-      console.error('Error fetching Omu Index:', error);
-  } finally {
-    process.stdin.setRawMode(false);  // stdinのrawモードを解除
-    process.stdin.pause();  // stdinを閉じる
-  }
-})();
+//(async () => {
+//  try {
+//    const result = await omuIndexMain(stationName); // 戻り値は表示する文字列
+//    console.log(result);
+//  } catch (error) {
+//      console.error('Error fetching Omu Index:', error);
+//  } finally {
+//    process.stdin.setRawMode(false);  // stdinのrawモードを解除
+//    process.stdin.pause();  // stdinを閉じる
+//  }
+//})();
